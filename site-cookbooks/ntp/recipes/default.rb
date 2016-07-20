b3ac6141b920252e7e9b66bd96f4bca233ca7ede
Diep Le <dieple1@gmail.com>
2016-07-20 15:25:29 +0100

    initial commit

---
+++ site-cookbooks/ntp/recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: ntp
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "ntp" do
+  action [:install]
+end
+
+ntp_server = data_bag_item('ntp', 'default_server')
+
+template "/etc/ntp.conf" do
+  source "ntp.conf.erb"
+  variables( :ntp_server => ntp_server['value'] )
+  notifies :restart, "service[ntpd]"
+end
+
+service "ntpd" do
+  action[:enable,:start]
+end
